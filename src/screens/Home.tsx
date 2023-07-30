29ad49a98df716f4f1a36441396b6cda9fdb654d
Felipe Fransa <antoniofelipesilva@live.com>
2023-07-30 15:47:41 -0300

    add style

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import {Text, View } from 'react-native';
+
+export const Home = () => {
+  return (
+    <View className='flex-1 bg-black justify-center items-center'>
+      <Text>Helo native!</Text>
+    </View>
+  );
+}
